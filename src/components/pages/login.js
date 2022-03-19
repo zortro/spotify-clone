db08c78c7f7860d20317e7acd2dbb966b2970255
ZETA <ben.r.dominguez@gmail.com>
2022-03-18 18:17:00 -0700

    completed login/signup styling

--- src/components/pages/login.js
+++ src/components/pages/login.js
@@ -1,18 +1,56 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Logo from '../assets/images/logo.png'
 
 export default function Login() {
+    const [sign, choice] = useState(false)
+
+    function login() {
+        choice(true)
+    }
+
+    function signup() {
+        choice(false)
+    }
+
     return (
         <div>
             <div className='loginCont'>
-                <h1>Log in to continue.</h1>
-                <input/>
-                <input/>
-                <button>Log In</button>
-                <div className='line'/>
-                <p>or</p>
-                <div className='line'/>
-                <button>Sign Up</button>
-                <button>RESET PASSWORD</button>
+                <img className='logo' src={Logo}/>
+                <div className='choice'>
+                    <h1 className={sign === true ? 'active choiceName' : 'choiceName'}>Log In</h1>
+                    <h1 className={sign === false ? 'active choiceName' : 'choiceName'}>Sign Up</h1>
+                </div>
+                <div className='choiceLine'/>
+            {sign === true ? (
+                <>
+                        <h1 className='loginText'>Log in to continue.</h1>
+                        <input placeholder='Username'/>
+                        <input placeholder='Password'/>
+                        <button className='logIn'>Log In</button>
+                        <div className='divider'>
+                            <div className='line'/>
+                            <p className='dividerText'>or</p>
+                            <div className='line'/>
+                        </div>
+                        <button className='else' onClick={signup}>Sign Up</button>
+                        <a href='#'>RESET PASSWORD</a>
+
+                </>
+            ) : (
+                <>
+                        <h1 className='loginText'>Sign up to continue.</h1>
+                        <input placeholder='Username'/>
+                        <input placeholder='Password'/>
+                        <button className='logIn'>Sign Up</button>
+                        <div className='divider'>
+                            <div className='line'/>
+                            <p className='dividerText'>or</p>
+                            <div className='line'/>
+                        </div>
+                        <button className='else' onClick={login}>Log In</button>
+                        <a href='#'>RESET PASSWORD</a>
+                </>
+            )}          
             </div>
         </div>
     )
